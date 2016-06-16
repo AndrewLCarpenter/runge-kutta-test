9e003fc8b218d95a81be018064ff587bb638a612
brandonmurray0 <bmurray7@binghamton.edu>
2016-06-16 14:42:28 -0400

    6-16-16 started to reoganize test_cases and how it works

---
+++ general_expanded/reworked/gammq.f90
@@ -0,0 +1,22 @@
+      FUNCTION gammq(a,x)
+
+      implicit none
+      
+      integer,  parameter        ::  wp=8
+
+      real(wp),   intent(in   )  ::  a,x
+      real(wp)                   ::  gammq
+
+      real(wp)                   :: gamser,gln,gammcf
+ 
+!     USES gcf,gser
+!     Returns the incomplete gamma function Q(a, x) = 1 -P(a, x).
+
+      if(x.lt.0..or.a.le.0.)pause 'bad arguments in gammq'
+      if (x.lt.a+1.) then 
+      call gser(gamser,a,x,gln)
+      gammq=1. -gamser 
+      call gcf(gammcf,a,x,gln)
+      endif
+      return
+      end
